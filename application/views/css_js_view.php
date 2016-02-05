74431db6efbf0d40f72d9afdbb0881ad29211f70
dimitryrakhlei <dimitryrakhlei@gmail.com>
2016-02-05 10:36:58 -0800

    got headers working

---
+++ application/views/css_js_view.php
@@ -0,0 +1,7 @@
+<head>
+<!--Load style.css file, which store in css folder.-->
+<link rel="icon" href="favicon.ico">
+<link rel="stylesheet" href="/assets/js/semantic.css">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="/assets/js/semantic.js"></script>
+</head>
