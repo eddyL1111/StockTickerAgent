e2b16206e1b92d6756e3ae67b408ae6deaf78c27
EddyL <kvl192@hotmail.com>
2016-02-13 19:33:06 -0800

    Overview code hyperlinks changed into submit buttons to redirect to the stock page, which is ordered by the selected code

--- application/views/css_js_view.php
+++ application/views/css_js_view.php
@@ -2,6 +2,7 @@
 <!--Load style.css file, which store in css folder.-->
 <link rel="icon" href="favicon.ico">
 <link rel="stylesheet" href="/assets/js/semantic.css">
+<link rel="stylesheet" href="/assets/CSS/overview.css">
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="/assets/js/semantic.js"></script>
 <script src="/assets/js/main.js"></script>
