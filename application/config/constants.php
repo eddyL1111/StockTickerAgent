7ecaa547d6e8cd792cc8c82762af081bbf475481
EddyL <kvl192@hotmail.com>
2016-04-07 22:44:35 -0700

    Defined URL for csv; folder path; user roles

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,28 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+
+/* ----------------------------------------------------
+ * User Roles
+ * ----------------------------------------------------
+ */
+define('ROLE_PLAYER', 'player');
+define('ROLE_ADMIN','admin');
+
+
+/* ----------------------------------------------------
+ * Folder path
+ * ----------------------------------------------------
+ */
+define('DATAPATH', './data/');
+define('XMLSUFFIX','.xml');
+
+/* ----------------------------------------------------
+ * URL For Server
+ * ----------------------------------------------------
+ */
+define('STATUSDATA_URL', 'http://bsx.jlparry.com/status');
+define('STOCKDATA_URL', 'http://bsx.jlparry.com/data/stocks');
+define('MOVEMENTDATA_URL', 'http://bsx.jlparry.com/data/movement');
+define('TRANSACTIONSDATA_URL', 'http://bsx.jlparry.com/data/transactions');
