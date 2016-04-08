06a902c190eb30331573443300674c31a778def3
EddyL <kvl192@hotmail.com>
2016-04-07 22:49:22 -0700

    Added the agent management view

---
+++ application/views/manager.php
@@ -0,0 +1,48 @@
+<h1>{page_title}</h1>
+
+<div class="ui center aligned two column grid">
+    <div class="column">
+        <h2>BSX Server</h2>
+        <div class="row">
+            <table class="ui celled table">
+            <tbody>
+                <tr>
+                    <td>Round</td>
+                    <td>{round}</td>
+                </tr>
+                <tr>
+                    <td>State</td>
+                    <td>{state}</td>
+                </tr>
+                <tr>
+                    <td>Countdown</td>
+                    <td>{countdown}</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+    </div>
+    
+    <div class="column">
+        <h2>Download Newest CSV Data</h2>
+        <div class="row">
+            <table class="ui celled table">
+            <tbody>
+                <tr>
+                    <td>Stocks</td>
+                    <td><a href="manager/download_stockdata_csv">Download</a></td>
+                </tr>
+                <tr>
+                    <td>Movement</td>
+                    <td><a href="manager/download_movement_csv">Download</a></td>
+                </tr>
+                <tr>
+                    <td>Transactions</td>
+                    <td><a href="manager/download_transactions_csv">Download</a></td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+    </div>
+    
+</div>
