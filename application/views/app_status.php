9aaca52a415ca2afb61a5e5647cda7019fc5f599
EddyL <kvl192@hotmail.com>
2016-04-17 09:04:24 -0700

    Page that displays app status if team form was sucessfully posted

---
+++ application/views/app_status.php
@@ -0,0 +1,8 @@
+<h3>App is connected!</h3>
+<p>Team: {team}</p>
+<p>Name: {name}</p>
+<p>Token: {token}</p>
+
+<form class="ui form" method="post" action="registration/disconnect">
+    <button class="ui button" type="submit">Disconnect</button>
+</form>
